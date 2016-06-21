db2e84af9341b92308f899ed8d923d4b7c375bf0
Cesar Cordeiro de Quadros <cquadros@gertec.local>
2016-06-21 16:36:11 -0300

    Criado projeto java no eclipse - Criado Objeto Produto

---
+++ src/model/Produtos.java
@@ -0,0 +1,37 @@
+package model;
+
+public class Produtos {
+
+	private int idProdutos;
+	private int idCategoria;
+	private String descricao;
+	private float preco;
+	
+	
+	public int getIdProdutos() {
+		return idProdutos;
+	}
+	public void setIdProdutos(int idProdutos) {
+		this.idProdutos = idProdutos;
+	}
+	public int getIdCategoria() {
+		return idCategoria;
+	}
+	public void setIdCategoria(int idCategoria) {
+		this.idCategoria = idCategoria;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public float getPreco() {
+		return preco;
+	}
+	public void setPreco(float preco) {
+		this.preco = preco;
+	}
+	
+	
+}
